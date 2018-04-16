5b95a7ed74b16a770b993e574cc3a285054fd1bd
Giuseppe Criscione <giuseppecriscionedev@gmail.com>
2018-04-16 16:46:54 +0200

    Classe Ram implementata

---
+++ ram.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <stringstream>
+#include "split.cpp"
+
+using namespace std;
+
+class Ram{
+
+public:	
+	int get_total(){
+		vector<string> v;
+		string s;
+		stringstream ss;
+		int ret;
+		ifstream file("/proc/meminfo");
+		v = split(file.getLine()," ");
+		s = v.at(v.length() - 2);
+		istringstream(s) >> ret;
+		return ret;
+	}
+
+	int get_used(){
+		vector<string> v;
+		string s;
+		stringstream ss;
+		int ret;
+		ifstream file("/proc/meminfo");
+		for(int i = 0; i < 2; i++)
+			file.getLine();	
+		v = split(file.getLine()," ");
+		s = v.at(v.length() - 2);
+		istringstream(s) >> ret;
+		return ret;
+
+	}
+
+
+	int get_used_percent(){
+		return (get_used()/get_total())*100);
+	}
+
+	
+
+	Ram(){}
+};
+
+
+
+
